20d284b872be691c510e3fba76eddf398756e5b7
Wesley Andrade <wafermonster@yahoo.com>
2024-11-19 11:26:10 +0700

    Update tradingPlan.py

--- tradingPlan.py
+++ tradingPlan.py
@@ -2,26 +2,27 @@ from tradingview_ta import Interval, get_multiple_analysis
 from prettytable import PrettyTable
 
 
+# symbols (list): List of EXCHANGE:SYMBOL (ex: ["NASDAQ:AAPL"] or ["BINANCE:BTCUSDT"] or ["FX_IDC:USDCAD"])
 WATCH_LIST = [
-    "AUDCAD",
-    "AUDCHF",
-    "AUDUSD",
-    "CADCHF",
-    "CADSGD",
-    "EURAUD",
-    "EURCAD",
-    "EURCHF",
-    "EURGBP",
-    "EURJPY",
-    "EURSGD",
-    "EURUSD",
-    "GBPJPY",
-    "GBPUSD",
-    "NZDUSD",
-    "USDCAD",
-    "USDCHF",
-    "USDTHB",
-    "ZARCHF",
+    "FX_IDC:AUDCAD",
+    "FX_IDC:AUDCHF",
+    "FX_IDC:AUDUSD",
+    "FX_IDC:CADCHF",
+    "FX_IDC:CADSGD",
+    "FX_IDC:EURAUD",
+    "FX_IDC:EURCAD",
+    "FX_IDC:EURCHF",
+    "FX_IDC:EURGBP",
+    "FX_IDC:EURJPY",
+    "FX_IDC:EURSGD",
+    "FX_IDC:EURUSD",
+    "FX_IDC:GBPJPY",
+    "FX_IDC:GBPUSD",
+    "FX_IDC:NZDUSD",
+    "FX_IDC:USDCAD",
+    "FX_IDC:USDCHF",
+    "FX_IDC:USDTHB",
+    "FX_IDC:ZARCHF",
 ]
 
 # order matters here
@@ -36,10 +37,6 @@ TIME_FRAMES = [
 
 
 def get_analysis_for_multiple_timeframes():
-    # add the exchange to all the symbols
-    # symbols (list): List of EXCHANGE:SYMBOL (ex: ["NASDAQ:AAPL"] or ["BINANCE:BTCUSDT"] or ["FX_IDC:USDCAD"])
-    symbols = [f"FX_IDC:{symbol}" for symbol in WATCH_LIST]
-    
     analysis = {}
 
     for time in TIME_FRAMES:
@@ -48,69 +45,68 @@ def get_analysis_for_multiple_timeframes():
                 analysis["M1"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_1_MONTH,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "W1":
                 analysis["W1"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_1_WEEK,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "D1":
                 analysis["D1"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_1_DAY,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "H4":
                 analysis["H4"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_4_HOURS,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "H2":
                 analysis["H2"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_2_HOURS,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "H1":
                 analysis["H1"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_1_HOUR,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "M30":
                 analysis["M30"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_30_MINUTES,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "M15":
                 analysis["M15"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_15_MINUTES,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "M5":
                 analysis["M5"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_5_MINUTES,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case "M1":
                 analysis["M1"] = get_multiple_analysis(
                     screener="forex",
                     interval=Interval.INTERVAL_1_MINUTE,
-                    symbols=symbols)
+                    symbols=WATCH_LIST)
             case _:
                 raise Exception("Invalid Time Frame")
 
     return analysis
 
-def print_table(all_analysis_data):
+def print_table(analysis_data):
     # Color
     R = "\033[0;31;40m"  # RED
     G = "\033[90m"  # Grey for Neutral
-    Y = "\033[0;33;40m"  # Yellow
     B = "\033[0;34;40m"  # Blue
     N = "\033[0m"  # Reset
-    
+   
     table = PrettyTable()
-    table.add_column("", WATCH_LIST)
+    table.add_column("", [item.split(":")[1] for item in WATCH_LIST])
 
-    for timeframe, analysis in all_analysis_data.items():
+    for timeframe, analysis in analysis_data.items():
         column_data = [
             B + value.summary["RECOMMENDATION"] + N
             if "BUY" in value.summary["RECOMMENDATION"]
