80ca410699151c85862b312c644e2a1bbcf91b03
Wesley Andrade <wafermonster@yahoo.com>
2024-11-19 11:08:47 +0700

    cleanup

--- tradingPlan.py
+++ tradingPlan.py
@@ -1,5 +1,4 @@
-from tradingview_ta import Analysis, TA_Handler, Interval, Exchange, get_multiple_analysis
-from colorama import Fore, Style
+from tradingview_ta import Interval, get_multiple_analysis
 from prettytable import PrettyTable
 
 
@@ -37,70 +36,95 @@ TIME_FRAMES = [
 
 
 def get_analysis_for_multiple_timeframes():
+    # add the exchange to all the symbols
+    # symbols (list): List of EXCHANGE:SYMBOL (ex: ["NASDAQ:AAPL"] or ["BINANCE:BTCUSDT"] or ["FX_IDC:USDCAD"])
+    symbols = [f"FX_IDC:{symbol}" for symbol in WATCH_LIST]
+    
     analysis = {}
 
     for time in TIME_FRAMES:
-        if time == "W1":
-            analysis["W1"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_1_WEEK,
-                symbols=symbols)
-        elif time == "D1":
-            analysis["D1"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_1_DAY,
-                symbols=symbols)
-        elif time == "H4":
-            analysis["H4"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_4_HOURS,
-                symbols=symbols)
-        elif time == "H1":
-            analysis["H1"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_1_HOUR,
-                symbols=symbols)
-        elif time == "M30":
-            analysis["M30"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_30_MINUTES,
-                symbols=symbols)
-        elif time == "M15":
-            analysis["M15"] = get_multiple_analysis(
-                screener="forex",
-                interval=Interval.INTERVAL_15_MINUTES,
-                symbols=symbols)
+        match time:
+            case "M1":
+                analysis["M1"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_1_MONTH,
+                    symbols=symbols)
+            case "W1":
+                analysis["W1"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_1_WEEK,
+                    symbols=symbols)
+            case "D1":
+                analysis["D1"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_1_DAY,
+                    symbols=symbols)
+            case "H4":
+                analysis["H4"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_4_HOURS,
+                    symbols=symbols)
+            case "H2":
+                analysis["H2"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_2_HOURS,
+                    symbols=symbols)
+            case "H1":
+                analysis["H1"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_1_HOUR,
+                    symbols=symbols)
+            case "M30":
+                analysis["M30"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_30_MINUTES,
+                    symbols=symbols)
+            case "M15":
+                analysis["M15"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_15_MINUTES,
+                    symbols=symbols)
+            case "M5":
+                analysis["M5"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_5_MINUTES,
+                    symbols=symbols)
+            case "M1":
+                analysis["M1"] = get_multiple_analysis(
+                    screener="forex",
+                    interval=Interval.INTERVAL_1_MINUTE,
+                    symbols=symbols)
+            case _:
+                raise Exception("Invalid Time Frame")
+
     return analysis
 
+def print_table(all_analysis_data):
+    # Color
+    R = "\033[0;31;40m"  # RED
+    G = "\033[90m"  # Grey for Neutral
+    Y = "\033[0;33;40m"  # Yellow
+    B = "\033[0;34;40m"  # Blue
+    N = "\033[0m"  # Reset
+    
+    table = PrettyTable()
+    table.add_column("", WATCH_LIST)
 
-# add the exchange to all the symbols
-symbols = [f"FX_IDC:{symbol}" for symbol in WATCH_LIST]
+    for timeframe, analysis in all_analysis_data.items():
+        column_data = [
+            B + value.summary["RECOMMENDATION"] + N
+            if "BUY" in value.summary["RECOMMENDATION"]
+            else R + value.summary["RECOMMENDATION"] + N
+            if "SELL" in value.summary["RECOMMENDATION"]
+            else G + "~~" + N
+            if "NEUTRAL" in value.summary["RECOMMENDATION"]
+            else value.summary["RECOMMENDATION"]  # Default case, no color
+            for key, value in analysis.items()
+        ]
+        table.add_column(timeframe, column_data)
 
-# Color
-R = "\033[0;31;40m"  # RED
-G = "\033[90m"  # Grey for Neutral
-Y = "\033[0;33;40m"  # Yellow
-B = "\033[0;34;40m"  # Blue
-N = "\033[0m"  # Reset
+    print(table)
 
-
-table = PrettyTable()
-
-table.add_column("", WATCH_LIST)
-
-all_analysis_data = get_analysis_for_multiple_timeframes()
-for timeframe, analysis in all_analysis_data.items():
-    column_data = [
-        B + value.summary["RECOMMENDATION"] + N
-        if "BUY" in value.summary["RECOMMENDATION"]
-        else R + value.summary["RECOMMENDATION"] + N
-        if "SELL" in value.summary["RECOMMENDATION"]
-        else G + "~~" + N
-        if "NEUTRAL" in value.summary["RECOMMENDATION"]
-        else value.summary["RECOMMENDATION"]  # Default case, no color
-        for key, value in analysis.items()
-    ]
-    table.add_column(timeframe, column_data)
-
-
-print(table)
+if __name__ == "__main__":
+    all_analysis_data = get_analysis_for_multiple_timeframes()
+    print_table(all_analysis_data)
